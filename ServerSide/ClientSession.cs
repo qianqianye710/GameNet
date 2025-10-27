64580c69e8e6b2dd07aecf7caf7ec6ce0825096b
qianqianye <3598912846@qq.com>
2025-10-27 21:43:40 +0800

    NetBase

---
+++ ServerSide/ClientSession.cs
@@ -0,0 +1,123 @@
+﻿using GameNet;
+using Google.Protobuf;
+using System.Net.Sockets;
+
+namespace ServerSide
+{
+    /// <summary>
+    /// 单个客户端类
+    /// </summary>
+    internal class ClientSession
+    {
+        private string playerName = "Player";
+        private TcpClient client;
+        private NetworkStream netDataStream;
+        private byte[] recvBuffer = new byte[4096];
+
+        /// <summary>
+        /// 客户端构造函数
+        /// </summary>
+        /// <param name="_client">连接对象</param>
+        /// <param name="_msgHandler">消息处理委托</param>
+        public ClientSession(TcpClient _client, Action<ClientSession, MsgType, byte[]> _msgHandler)
+        {
+            client = _client;
+            netDataStream = client.GetStream();
+            StartReceive(_msgHandler);
+        }
+
+        /// <summary>
+        /// 启动异步接受客户端消息
+        /// </summary>
+        /// <param name="msgHandler"></param>
+        private void StartReceive(Action<ClientSession, MsgType, byte[]> msgHandler)
+        {
+            //开始读取
+            netDataStream.BeginRead(
+                recvBuffer,
+                0,
+                recvBuffer.Length,
+                (ar) =>
+                {
+                    try
+                    {
+                        //结束读取
+                        int byteRead = netDataStream.EndRead(ar);
+                        if (byteRead <= 0)
+                        {
+                            //断开连接
+                            DisConnect();
+                            return;
+                        }
+                        //解析消息
+                        int offset = 0;//当前解析偏移量
+                        while (offset + 3 <= byteRead)
+                        {
+                            //解析消息长度
+                            ushort len = (ushort)((recvBuffer[offset] << 8) | recvBuffer[offset + 1]);
+                            if (offset + 3 + len > byteRead) break;
+
+                            //解析消息
+                            MsgType msgType = (MsgType)recvBuffer[offset + 2];
+                            byte[] data = new byte[len];
+                            Array.Copy(recvBuffer, offset + 3, data, 0, len);
+
+                            //调用消息处理委托
+                            msgHandler(this, msgType, data);
+                            offset += 3 + len;
+                        }
+
+                        //异步接收下一批数据
+                        StartReceive(msgHandler);
+                    }
+                    catch
+                    {
+                        DisConnect();
+                    }
+                },
+                null
+            );
+        }
+
+        /// <summary>
+        /// 发送消息方法
+        /// </summary>
+        /// <param name="msgType">消息类型</param>
+        /// <param name="msg">序列化消息对象</param>
+        public void SendMsg(MsgType msgType,IMessage msg)
+        {
+            try
+            {
+                //序列话消息对象
+                byte[] data = msg.ToByteArray();
+                ushort len = (ushort)data.Length;
+                //构建消息包
+                byte[] buffer = new byte[len + 3];
+                buffer[0] = (byte)(len >> 8); // 长度高8位
+                buffer[1] = (byte)(len & 0xFF); // 长度低8位 
+                buffer[2] = (byte)msgType;
+                Array.Copy(data, 0, buffer, 3, len);
+
+                netDataStream.Write(buffer, 0, buffer.Length);
+            }
+            catch
+            {
+                DisConnect();
+            }
+        }
+
+        /// <summary>
+        /// 断开连接
+        /// </summary>
+        public void DisConnect()
+        {
+            netDataStream?.Close();
+            client?.Close();
+            lock (Server._lock)
+            {
+                Server.sessions.Remove(this);
+            }
+            Console.WriteLine($"客户端:{playerName}断开连接");
+        }
+    }
+}
---
+++ ServerSide/ServerSide.cs
@@ -0,0 +1,100 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Google.Protobuf;
+using GameNet;
+using MySqlConnector;
+using BCrypt;
+
+namespace ServerSide
+{
+    /// <summary>
+    /// 服务器端
+    /// </summary>
+    internal class Server
+    {
+        private static TcpListener listener = null!;
+        internal static List<ClientSession> sessions = new();
+
+        internal static object _lock = new object();
+
+        /// <summary>
+        /// 主程序
+        /// </summary>
+        static void Main()
+        {
+            listener = new TcpListener(IPAddress.Any, 1220);
+            listener.Start();
+            Console.WriteLine("服务端启动，端口号:1220");
+
+            while (true)
+            {
+                //阻塞等待客户端连接
+                TcpClient client = listener.AcceptTcpClient();
+                lock (_lock)
+                {
+                    //创建客户端对话对象，传入client和消息处理方法
+                    sessions.Add(new ClientSession(client, HandleMsg));
+                }
+                //输出客户端连接信息，当前在线客户端数量
+                Console.WriteLine($"客户端:{client}已连接，当前在线：{sessions.Count}");
+            }
+        }
+
+        /// <summary>
+        /// 处理客户端消息
+        /// </summary>
+        /// <param name="session">发送消息的客户端</param>
+        /// <param name="msgType">消息类型</param>
+        /// <param name="data">消息体</param>
+        private static void HandleMsg(ClientSession session,MsgType msgType, byte[] data)
+        {
+            switch (msgType)
+            {
+                case MsgType.LoginReq:
+                    //反序列化消息为LoginRequest对象
+                    var loginReq = LoginRequest.Parser.ParseFrom(data);
+                    //创建登录响应对象
+                    var loginRes = new LoginResponse
+                    {
+                        Success = CheckLoginInfo(loginReq)
+                    };
+                    loginRes.Msg = loginRes.Success ? "登陆成功" : "账号密码错误";
+                    //给客户端发送登录结果消息
+                    session.SendMsg(MsgType.LoginRsp, loginRes);
+                    break;
+                //case MsgType.SyncPos:
+                    //var syvcPos = SyncPosition.Parser.ParseFrom(data);
+                    //广播位置消息
+                    //BroadCast(syvcPos, session);
+            }
+        }
+        /// <summary>
+        /// 广播消息
+        /// </summary>
+        /// <param name="msg">消息体</param>
+        /// <param name="session">广播客户端</param>
+        private static void BroadCast(IMessage msg, ClientSession session)
+        {
+            lock (_lock)
+            {
+                foreach(var sso in sessions)
+                {
+                    //判断是否为发送客户端
+                    if (sso != session)
+                    {
+                        //sso.SendMsg(MsgType.SyncPos, msg);
+                    }
+                }
+            }
+        }
+
+        private static bool CheckLoginInfo(LoginRequest loginReq)
+        {
+            if (loginReq.Username == "test" && loginReq.Password == "1220")
+                return true;
+            else if (loginReq.Username == "gu" && loginReq.Password == "1026")
+                return true;
+            return false;
+        }
+    }
+}
